ac272921017f77a9ddce6de9f2805aa594fb3a7c
setterwall <RobinSetterwall@n178-p226.kthopen.kth.se>
2015-04-23 15:06:05 +0200

    Final

--- app.js
+++ app.js
@@ -1,70 +1,89 @@
 //1. Vid pageload: Hitta feed, skapa upp objekt med all data för varje nyhet, lägg i array och kör generateHome('dataObjArray')
+
+
 //2. Då användaren valt en kategori: radera data från homeView (removeContent('homeView')), generera och visa listView
+
+
 //3. Då användaren klickar på en nyhet från listView: radera data i list (removeContent('listView')), ladda detail view (generateDetail('dataObj')), hämta nya viewen (showView('detailView'))
 
+var dataObjArray = [];
 
 
+function listCategory(category){
+      showView('listView');
+      loadCategory(category);
 
-//Creates the home view and all the categories presented from the feed, parameter is dataObjArray
-function generateHome(){
+}
 
-dataObjArray = [{image:"some image", category:"movies", title:"a title", description:"a description", author:"an author", date:"16/4"}, {category: '2'}, {category:'3'}];
+function listDetails(newsNumber){
+      showView('detailView');
+      loadDetails(newsNumber);
+}
 
-	var categoryArray = [];
-	for (var i = 0; i<dataObjArray.length; i++) {
-		categoryArray.push(dataObjArray[i].category);
-	}
+//Generates the detail view by adding content to the divs
+function loadDetails(newsNumber) {
+	var image = document.getElementById('imageDiv');
+	var title = document.getElementById('titleDiv');
+	var date = document.getElementById('dateDiv');
+   var link = document.getElementById('linkDiv');
+   var category = document.getElementById('categoryDetail');
 
-
-		home = document.getElementById('homeView');
-		catDiv = document.createElement('div');
-		catDiv.setAttribute('id', 'catDiv');
-		home.appendChild(catDiv);
-		for (var i = 0; i<categoryArray.length; i++) {
-			var newCategory = document.createElement('button');
-			newCategory.setAttribute("class", "btn btn-primary");
-			newCategory.setAttribute("onClick", "generateList('" + categoryArray[i] + "')");
-			newCategory.innerHTML = categoryArray[i];
-			catDiv.appendChild(newCategory);
-	}
-	}
-//Generates the detail view by adding content to the divs, the parameter is an object containg all the data for the news feed
-function generateDetail() {
-	dataObj = {image:"some image", category:"movies", title:"a title", description:"a description", author:"an author", date:"16/4"};
-	image = document.getElementById('imageDiv');
-	category = document.getElementById('categoryDiv');
-	title = document.getElementById('titleDiv');
-	description = document.getElementById('descriptionDiv');
-	author = document.getElementById('authorDiv');
-	date = document.getElementById('dateDiv');
-
-	image.innerHTML = dataObj.image;
-	category.innerHTML = dataObj.category;
-	title.innerHTML = dataObj.title;
-	description.innerHTML = dataObj.description;
-	author.innerHTML = dataObj.author;
-	date.innerHTML = dataObj.date;
+	console.log(title+ " details: "+newsNumber);
+	image.innerHTML = "<img class='img-responsive' class ='img-rounded' src="+dataObjArray[newsNumber].image+" alt='Chania' width='460' height='345'>";
+	category.innerHTML = "<h2>About <b><sub id = 'headerCategory'>"+dataObjArray[newsNumber].category+"</sub></b></h2>";
+	title.innerHTML = "<h4>"+dataObjArray[newsNumber].title+"</h4>";
+	date.innerHTML = dataObjArray[newsNumber].pubDate;
+   link.innerHTML = "<h4><a href = "+dataObjArray[newsNumber].link+">Go to page of article</a></h4>";
 }
 
 
-//Removes content from the views
-function removeContent(view){
-	if (view == 'homeView'){
-			$('#catDiv').remove()
-	}
-	else if (view == 'listView'){
-		alert('hej');
-	}
-	else if (view == 'detailView'){
-		$('#imageDiv').empty();
-		$('#categoryDiv').empty();
-		$('#titleDiv').empty();
-		$('#descriptionDiv').empty();
-		$('#authorDiv').empty();
-		$('#dateDiv').empty();
-		
-	}
+
+//Function that loads and displays a list of the
+//news in a given category
+function loadCategory(category){
+            
+      dataObjArray = []; //clean
+
+      var title;
+      var pubDate; 
+      var link;
+      var image;
+
+      var title = document.getElementById('categoryTitle'); 
+      title.innerHTML = "<h2>News from <b><sub id = 'headerCategory'>"+category+"</sub></b></h2>";
+
+      var feed = new google.feeds.Feed('http://www.rendip.com/c/'+category+'/rss');
+      feed.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
+      feed.setNumEntries(20);
+      feed.load(function(result) {
+        if (!result.error) {
+          var container = document.getElementById("feed");				//TODO
+        //  for (var i = 0; i < result.feed.entries.length; i++) {	//Loop in the other direction to get the news in order by date
+		    for (var i = result.feed.entries.length-1; i >= 0; i--) {	            
+				var entry = result.feed.entries[i];
+               title = entry.title;
+               pubDate = entry.publishedDate;
+               var tmp = pubDate.split(" ");
+               pubDate = tmp[0] +" "+ tmp[1] +" "+ tmp[2] +" "+ tmp[3];
+               link = entry.link;
+               var mediaEntries = entry.xmlNode.getElementsByTagNameNS('*','thumbnail');
+                   for (var j = 0; j < mediaEntries.length; j++) {
+                    image = mediaEntries[j].attributes.getNamedItem('url').value
+              }
+            
+            //Store the news as an object
+            dataObjArray.push({title : title, pubDate : pubDate, link: link, category: category, image: image});  
+				//dataObjArray.reverse();
+            //Display the news to the listView
+				var tmp = (result.feed.entries.length-1)-i;
+				console.log(title+ " number: "+tmp);
+            $("tbody").prepend("<tr><th onclick = listDetails("+tmp+")>"+title+"</th><th>"+pubDate+"</th></tr>");
+          }
+	
+        }
+      });
 }
+
 //Toggle between the different views
 	function showView(goto) {
 		var views = document.getElementsByClassName('views');
@@ -72,12 +91,6 @@ function removeContent(view){
 			views[i].style.display = 'none';
 			}
 		var next = document.getElementById(goto);
-		next.style.display = "block";
-		if (goto == 'homeView') {
-			generateHome(categories);
-
-		}
-		if (goto == 'detailView') {
-			generateDetail();
-		}
+		next.style.display = 'block';
 	}
+
